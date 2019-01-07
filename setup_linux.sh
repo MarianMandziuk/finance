0c3c0444413abdef73ab1242a022a10639e41e7b
Marian Mandziuk <maryan.mandziuk@gmail.com>
2019-01-07 13:04:51 +0200

    Update setup

--- setup_linux.sh
+++ setup_linux.sh
@@ -5,4 +5,6 @@ git clone https://github.com/MarianMandziuk/finance.git
 cd finance
 pip install -r requirements.txt
 cd fin
+python manage.py migrate
+echo '---------------Open link http://127.0.0.1:8000/finance --------------------'
 python manage.py runserver
